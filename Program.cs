cb4ea85999cbaef3d9c3e078222b7b7bf50bbaf8
kulikp <kulik_piotr@o2.pl>
2021-04-21 12:25:49 +0000

    first

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace githubpit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
